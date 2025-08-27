d88bd14636241f151e77d0381a209078df496b05
ElWali <capmedia@gmail.com>
2025-08-27 02:11:47 +0100

    Update Expression.js

--- src/core/Expression.js
+++ src/core/Expression.js
@@ -1,1 +1,29 @@
+export class Expression {
+  static evaluate(expr, feature) {
+    if (!Array.isArray(expr)) return expr;
 
+    const [op, ...args] = expr;
+
+    switch (op) {
+      case 'get':
+        return feature.properties[args[0]];
+      case 'interpolate':
+        return this.interpolate(args, feature);
+      case 'age':
+        return Date.now() - feature.properties.timestamp;
+      default:
+        console.warn(`Unknown expression operator: ${op}`);
+        return null;
+    }
+  }
+
+  static interpolate([type, input, ...stops], feature) {
+    const value = this.evaluate(input, feature);
+    for (let i = 0; i < stops.length; i += 2) {
+      if (value <= stops[i]) {
+        return stops[i + 1];
+      }
+    }
+    return stops[stops.length - 1];
+  }
+}
