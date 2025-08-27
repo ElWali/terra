6d772308b42d369e914bb87eb2823190af3f9652
ElWali <capmedia@gmail.com>
2025-08-27 02:07:38 +0100

    Update View.js

--- src/core/View.js
+++ src/core/View.js
@@ -1,1 +1,27 @@
+import { Renderer } from './Renderer.js';
 
+export class View {
+  constructor({ container, layers }) {
+    this.container = document.getElementById(container);
+    this.layers = layers || [];
+    this.renderer = new Renderer(this.container);
+
+    this.initLayers();
+  }
+
+  initLayers() {
+    this.layers.forEach(layer => {
+      this.renderer.addLayer(layer);
+      if (layer.source) {
+        layer.source.subscribe(data => {
+          this.renderer.updateLayer(layer.id, data);
+        });
+      }
+    });
+  }
+
+  addLayer(layer) {
+    this.layers.push(layer);
+    this.renderer.addLayer(layer);
+  }
+}
