981e228f455a3ef829a08008288579c05d488a34
ElWali <capmedia@gmail.com>
2025-08-27 02:12:54 +0100

    Update SymbolLayer.js

--- src/layers/SymbolLayer.js
+++ src/layers/SymbolLayer.js
@@ -1,1 +1,18 @@
+import { Expression } from '../core/Expression.js';
 
+export class SymbolLayer {
+  constructor({ id, source, layout, paint }) {
+    this.id = id;
+    this.source = source;
+    this.layout = layout;
+    this.paint = paint;
+  }
+
+  getLayoutProperty(property, feature) {
+    return Expression.evaluate(this.layout[property], feature);
+  }
+
+  getPaintProperty(property, feature) {
+    return Expression.evaluate(this.paint[property], feature);
+  }
+}
